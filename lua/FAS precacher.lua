debe1daf0b2c8b50c028b7589610eae5dffffc41
Piehthyte <Brigmanzander@gmail.com>
2014-07-30 18:38:41 -0500

    Converted tabs to spaces

--- lua/FAS precacher.lua
+++ lua/FAS precacher.lua
@@ -2,46 +2,46 @@
 print("Notice for the FA:S Precacher Addon\nI take NO CREDIT for the precacher script in this addon, all credit goes to the original author!");
 
 function PrecacheDir( dir ) // Recursively adds everything in a directory to be cached
-	local files, directories = file.Find( dir.."*", "GAME" )
-	for _, fdir in pairs(directories) do
-		if fdir != ".svn" then // Don't spam people with useless .svn folders'
-			PrecacheDir(dir..fdir.."/")
-		end
-	end
+    local files, directories = file.Find( dir.."*", "GAME" )
+    for _, fdir in pairs(directories) do
+        if fdir != ".svn" then // Don't spam people with useless .svn folders'
+            PrecacheDir(dir..fdir.."/")
+        end
+    end
 
-	for k,v in pairs(files) do
-		local fname = string.lower(dir..v)
-				print(fname)
-		local ismodel = -1
-		local ismaterial = -1
-		local isparticle = -1
-		local issound = -1
-		ismodel = (string.find(fname,".mdl"))
-		ismaterial = (string.find(fname,".vtf") or string.find(fname,".vmt"))
-		isparticle = (string.find(fname,".pcf"))
-		issound = (string.find(fname,".wav") or string.find(fname,".mp3")  )
-		if ismaterial then
-			if ismaterial >= 0 then
-				local tmpmat = Material(fname,"mips")
-			end
-		elseif isparticle then
-			if isparticle >= 0 then
+    for k,v in pairs(files) do
+        local fname = string.lower(dir..v)
+                print(fname)
+        local ismodel = -1
+        local ismaterial = -1
+        local isparticle = -1
+        local issound = -1
+        ismodel = (string.find(fname,".mdl"))
+        ismaterial = (string.find(fname,".vtf") or string.find(fname,".vmt"))
+        isparticle = (string.find(fname,".pcf"))
+        issound = (string.find(fname,".wav") or string.find(fname,".mp3")  )
+        if ismaterial then
+            if ismaterial >= 0 then
+                local tmpmat = Material(fname,"mips")
+            end
+        elseif isparticle then
+            if isparticle >= 0 then
 
-				PrecacheParticleSystem(fname)
-			end
-		elseif issound then
-			if issound >= 0 then
-				util.PrecacheSound(fname)
-			end
-		else
+                PrecacheParticleSystem(fname)
+            end
+        elseif issound then
+            if issound >= 0 then
+                util.PrecacheSound(fname)
+            end
+        else
 
-			if ismodel then
-				if ismodel >= 0 then
-					util.PrecacheModel(fname)
-				end
-			end
-		end
-	end
+            if ismodel then
+                if ismodel >= 0 then
+                    util.PrecacheModel(fname)
+                end
+            end
+        end
+    end
 end
 
 if SERVER then
